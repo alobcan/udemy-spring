d25e7b8135e8c8f96fee24462b07c648bd9cc4e9
Alejandro Obando <alejandro.obando@unosquare.com>
2023-02-07 10:51:05 -0400

    Added @Component annotation as another way to get Bean instances in Spring (Lesson 20)

--- demoproject/src/main/java/com/alobcan/config/ProjectConfig.java
+++ demoproject/src/main/java/com/alobcan/config/ProjectConfig.java
@@ -2,41 +2,12 @@ package com.alobcan.config;
 
 import com.alobcan.beans.Vehicle;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
 @Configuration
+@ComponentScan(basePackages = "com.alobcan.beans")
 public class ProjectConfig {
 
-    @Bean(name = "audiVehicle")
-    Vehicle vehicle1() {
-        var veh = new Vehicle();
-        veh.setName("Audi");
-        return veh;
-    }
-
-    @Bean(name = "hondaVehicle")
-    Vehicle vehicle2() {
-        var veh = new Vehicle();
-        veh.setName("Honda");
-        return veh;
-    }
-
-    @Primary
-    @Bean(name = "mazdaVehicle")
-    Vehicle vehicle3() {
-        var veh = new Vehicle();
-        veh.setName("Mazda");
-        return veh;
-    }
-
-    @Bean
-    String hello() {
-        return "Hello World";
-    }
-
-    @Bean
-    Integer number() {
-        return 16;
-    }
 }
--- demoproject/src/main/java/com/alobcan/main/Example1.java
+++ demoproject/src/main/java/com/alobcan/main/Example1.java
@@ -7,29 +7,11 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Example1 {
 
     public static void main(String[] args) {
-        Vehicle vehicle = new Vehicle();
-        vehicle.setName("Honda City");
-        System.out.println("Vehicle name for non-spring context is: " + vehicle.getName());
-
 
         var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
 
-        Vehicle veh = context.getBean("audiVehicle", Vehicle.class);
-        System.out.println("Vehicle name from Spring Context is: " + veh.getName());
-
-        Vehicle veh2 = context.getBean("hondaVehicle", Vehicle.class);
-        System.out.println("Vehicle name from Spring Context is: " + veh2.getName());
-
-        Vehicle veh3 = context.getBean("mazdaVehicle", Vehicle.class);
-        System.out.println("Vehicle name from Spring Context is: " + veh3.getName());
-
-        Vehicle primaryVehicle = context.getBean(Vehicle.class);
-        System.out.println("Primary Vehicle Bean name from Spring Context is: " + primaryVehicle.getName());
-
-        String hello = context.getBean(String.class);
-        System.out.println("String value from Spring Context is: " + hello);
-
-        Integer num = context.getBean(Integer.class);
-        System.out.println(("Integer value from Spring Context is: " + num));
+        Vehicle vehicle = context.getBean(Vehicle.class);
+        System.out.println("Component vehicle name from Spring Context is: " + vehicle.getName());
+        vehicle.printHello();
     }
 }

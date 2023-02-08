dcaedf80931893a2e09ee79902f150342562e71c
Alejandro Obando <alejandro.obando@unosquare.com>
2023-02-08 00:05:13 -0400

    Deepdive on Singleton Bean scope (Lesson 39)

--- demoproject/src/main/java/com/alobcan/main/Example1.java
+++ demoproject/src/main/java/com/alobcan/main/Example1.java
@@ -3,6 +3,7 @@ package com.alobcan.main;
 import com.alobcan.beans.Person;
 import com.alobcan.beans.Vehicle;
 import com.alobcan.config.ProjectConfig;
+import com.alobcan.services.VehicleServices;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -12,11 +13,13 @@ public class Example1 {
     public static void main(String[] args) {
 
         var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
-        Person person = context.getBean(Person.class);
-        Vehicle vehicle = context.getBean(Vehicle.class);
-        System.out.println("Person name from Spring Context is: " + person.getName());
-        System.out.println("Vehicle name from Spring Context is: " + vehicle.getName());
-        System.out.println("Vehicle that Person own is: " + person.getVehicle());
-        person.useCar();
+        VehicleServices vehicleServices1 = context.getBean(VehicleServices.class);
+        VehicleServices vehicleServices2 = context.getBean("vehicleServices", VehicleServices.class);
+
+        System.out.println("Hashcode of vehicleService1: " + vehicleServices1.hashCode());
+        System.out.println("Hashcode of vehicleService2: " + vehicleServices2.hashCode());
+        if (vehicleServices2 == vehicleServices1) {
+            System.out.println("Vehicle Service is a singleton");
+        }
     }
 }
--- demoproject/src/main/java/com/alobcan/services/VehicleServices.java
+++ demoproject/src/main/java/com/alobcan/services/VehicleServices.java
@@ -3,9 +3,12 @@ package com.alobcan.services;
 import com.alobcan.beans.Speakers;
 import com.alobcan.beans.Tyres;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 @Service
+@Scope(BeanDefinition.SCOPE_SINGLETON)
 public class VehicleServices {
     @Autowired
     public Speakers speakers;
